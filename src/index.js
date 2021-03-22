ce8f4ae62c8d1bd6a068ba2f7f5653963565839e
Claudio <cayoarenas14@gmail.com>
2021-03-22 17:49:02 -0300

    Actualiza nombre de carpeta dist para subir a github pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { saludar } from './js/componentes';
 import './styles.css';
 
 import img from './assets/logo-webpack.png';
-import img2 from './assets/Dimensiones_FKS145.jpg';
+
 
 const nombre = 'Claudio';
 
